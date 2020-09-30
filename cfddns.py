0fcf17b2a12871622ed19022b9005875db47a583
tomh4x <tk.metacognition@gmail.com>
2020-09-30 03:08:11 -0400

    swapped logging module for syslog

--- cfddns.py
+++ cfddns.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
-import cloudflare, logging, os, sys, json
+import cloudflare, os, sys, json
+from syslog import syslog, LOG_INFO
 from cfconfig import *
 from getAddr import getAddrFromPub
 from time import sleep
-from signal import signal, SIGINT
+from signal import signal, SIGINT, SIGTERM
 
-def sigint_handler(sig, frame):
-    logging.info('SIGINT Received: Exit.')
-    logging.shutdown()
+def sig_handler(sig, frame):
+    syslog( LOG_INFO, 'Signal {} Received: Exit.'.format(sig) )
     sys.exit(0)
 
 if __name__ == '__main__':
@@ -17,16 +17,16 @@ if __name__ == '__main__':
         sys.exit(0)
     
     # catch ctrl-c
-    signal(SIGINT, sigint_handler)
+    signal(SIGINT, sig_handler)
+    signal(SIGTERM, sig_handler)
 
     config = confLoad( sys.argv[1] )
     if not config:
-        print('Failed to load config file.  Bailing out')
+        print('Failed to load config file.  Bailing out', file=sys.stderr)
         sys.exit(-1)
 
-    logging.basicConfig(filename=confGetLogPath(config), format='[%(asctime)s] %(message)s', datefmt='%b %d %Y %H:%M:%S', level=logging.INFO)
-    logging.info('logging instantiated')
-    #instantiate cloudflare dns class and load config
+    syslog(LOG_INFO, 'logging initialized')
+    # instantiate cloudflare dns class and load config
     ddom = cloudflare.cfdns()
 
     # set creds
@@ -43,7 +43,7 @@ if __name__ == '__main__':
                     'proxied': False }
 
         current_rec = {'type':'A', 'name': confGetHost(config) }
-        logging.info('checking for existing host...')
+        syslog( LOG_INFO, 'checking for existing host...')
 
         # check for A record of the host assigned from config and current IP:
         recID = ddom.get_recordID( {'type':'A', \
@@ -53,51 +53,53 @@ if __name__ == '__main__':
 
         if recID:
             # everything is all good.  re-iterate event loop
-            logging.info( 'A record is set.  Waiting {} seconds...'.format( confGetInterval(config) ))
+            syslog( LOG_INFO,  'A record is set.  Waiting {} seconds...'.format( confGetInterval(config) ))
             sleep( confGetInterval(config) )
             next
             
         else:
-            logging.info('Existing host was missing or incorrect...')
+            syslog( LOG_INFO, 'Existing host was missing or incorrect...')
             # something isn't good
             # check if host exists regardless of IP
             ret = ddom.list_record( current_rec )
+            if not ret:
+                syslog( LOG_INFO, 'no http response!')
 
             # if we got a status 200 response with at least 1 result, it exists!
             if ret and ret.status_code==200 and len(ret.json()['result']) > 0:
-                logging.info('Outdated IP.  Updating...')
+                syslog( LOG_INFO, 'Outdated IP.  Updating...')
                 # we probably just need to update the record
                 upret = ddom.update_record( new_rec )
                 if upret and upret.status_code == 200:
                     next
                 else:
-                    logging.info('Failed to update record.  Something is wrong.  Bailing out!')
+                    syslog( LOG_INFO, 'Failed to update record.  Something is wrong.  Bailing out!')
                     sys.exit(-1)
             
             # bad API key.  chk_creds() should catch this
             elif ret and ret.status_code == 401:
-                logging.info('Authentication failure.  Bailing out!')
+                syslog( LOG_INFO, 'Authentication failure.  Bailing out!')
                 sys.exit(-1)
 
             # something else that makes no sense happened
             elif ret and ret.status_code != 200:
-                logging.info('HTTP Response code: {}: try again next time.'.format( ret.status_code) )
+                syslog( LOG_INFO, 'HTTP Response code: {}: try again next time.'.format( ret.status_code) )
                 sys.exit(-1)
 
             # everything is fine except the record doesn't exist so make it.
             elif ret and ret.status_code == 200 and len(ret.json()['result']) == 0:
-                logging.info('Record does not exist!  Creating...')
+                syslog( LOG_INFO, 'Record does not exist!  Creating...')
                 ret = ddom.add_record( new_rec)
                 if ret and ret.status_code == 200:
-                    logging.info('Record creation completed!')
+                    syslog( LOG_INFO, 'Record creation completed!')
                     # record should detect on next iteration and wait nicely
                     next
 
             # catchall
             else:
                 # probably no http response
-                logging.info('Something went very wrong!  No http response received!')
-                logging.info('Bailing out!')
+                syslog( LOG_INFO, 'Something went very wrong!  No http response received!')
+                syslog( LOG_INFO, 'Bailing out!')
                 sys.exit(-1)
     # END EVENT LOOP
     sys.exit(0)
